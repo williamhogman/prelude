76c8d6fbed61b0dc758f9ffac64ef6c9da8171fc
William Rudenmalm <me@whn.se>
2015-03-15 14:06:39 +0100

    Add default path for old .customize system

--- personal/preload/load-old-system.el
+++ personal/preload/load-old-system.el
@@ -1,3 +1,7 @@
+;; If C_HOME hasn't been set give it a resonable default
+(if (not (getenv "C_HOME"))
+    (setenv "C_HOME" (concat (getenv "HOME") "/.customize/")))
+
 ;; Load from .customize
 (add-to-list
  'load-path
