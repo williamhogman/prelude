3d5d77274e579205ab3af75a9f17e9d12a8da640
William Hogman <me@whn.se>
2015-03-15 09:26:25 +0100

    add settings for searching

---
+++ personal/searching.el
@@ -0,0 +1,18 @@
+;;; searching.el --- Configuration for searching stuff
+;;; Commentary:
+;;; Everything related to finding stuff across the code base.
+;;; Code:
+
+(prelude-require-package 'helm-ag)
+
+;;; Settings from prelude-helm-everywhere related to projectile
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
+(defun projectile-helm-ag ()
+  (interactive)
+  (helm-ag (projectile-project-root)))
+
+(define-key helm-command-map (kbd "s") 'projectile-helm-ag)
+
+;;; searching.el ends here
