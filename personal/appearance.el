4e22e6b58a687513c5ed3235c5e0809f0034d5b1
William Hogman <me@whn.se>
2015-03-15 08:52:08 +0100

    add smart-mode-line, remove scrolbars

--- personal/appearance.el
+++ personal/appearance.el
@@ -1,1 +1,18 @@
+;;; appearance.el --- Make emacs pretty
+;;; Commentary:
+;;; Configures the looks of my emacs installation
+;;; Code:
+
+;; No scrollbars ever
 (scroll-bar-mode -1)
+
+;; Configure smart-mode line to make it prettier
+(prelude-require-package 'smart-mode-line)
+(setq sml/theme nil
+      sml/shorten-directory t
+      sml/name-width 32
+      sml/shorten-modes t
+      sml/use-projectile-p 'before-prefixes
+      sml/projectile-replacement-format "%s/"
+      )
+(sml/setup)
