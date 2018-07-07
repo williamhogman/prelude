7ab5ea51c0ac16e0c85a41c3fa5d47ddfd900653
William Rudenmalm <me@whn.se>
2018-07-07 18:00:55 +0200

    improve mac-appearence

--- personal/preload/theme.el
+++ personal/preload/theme.el
@@ -1,2 +1,4 @@
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . nil))
 (disable-theme 'zenburn)
 (setq prelude-theme 'solarized-dark)
